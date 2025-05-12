636b442ab7de3daf8ed85f7567920932dae56a0d
Ommooley10 <omvikas1001@gmail.com>
2025-05-12 21:53:04 +0530

    updated 3d training pipeline

--- Training/3D_CNN_Training.py
+++ Training/3D_CNN_Training.py
@@ -1,21 +1,19 @@
 # train_3dcnn.py
 
-import pickle
 import tensorflow as tf
 from tensorflow.keras import layers, models
+from preprocess_3dcnn import data  # assuming both scripts are in the same folder
 
-# Load preprocessed data
-with open("/content/drive/MyDrive/land-cover-classification-master/train_data_3d.pkl", "rb") as f:
-    X_train, y_train = pickle.load(f)
+# Extract data
+X_train = data["X_train"]
+y_train = data["y_train"]
+X_val = data["X_val"]
+y_val = data["y_val"]
 
-with open("/content/drive/MyDrive/land-cover-classification-master/val_data_3d.pkl", "rb") as f:
-    X_val, y_val = pickle.load(f)
-
-# Model definition
+# Model architecture
 def build_3d_cnn(input_shape, num_classes):
     model = models.Sequential([
         layers.Input(shape=input_shape),
-
         layers.Conv3D(32, (3, 3, 3), activation='relu', padding='same'),
         layers.BatchNormalization(),
         layers.MaxPooling3D((2, 2, 2)),
@@ -32,17 +30,13 @@ def build_3d_cnn(input_shape, num_classes):
     model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     return model
 
-# Build model
-input_shape = X_train.shape[1:]  # (PATCH_SIZE, PATCH_SIZE, NUM_COMPONENTS, 1)
-num_classes = y_train.shape[1]
-model = build_3d_cnn(input_shape, num_classes)
+# Build and train model
+model = build_3d_cnn(X_train.shape[1:], y_train.shape[1])
 model.summary()
 
-# Callbacks
 early_stop = tf.keras.callbacks.EarlyStopping(patience=8, restore_best_weights=True)
 reduce_lr = tf.keras.callbacks.ReduceLROnPlateau(patience=4, factor=0.5)
 
-# Train
 history = model.fit(
     X_train, y_train,
     validation_data=(X_val, y_val),
@@ -52,5 +46,5 @@ history = model.fit(
     verbose=2
 )
 
-# Save model
+# Save trained model
 model.save("/content/drive/MyDrive/land-cover-classification-master/Models/3D CNN/3d_CNN_updated.h5")
